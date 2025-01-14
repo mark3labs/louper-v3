7ccdd423ca195a861b88c24d4d1a79b70ee2b049
Ed Zynda <ezynda3@gmail.com>
2025-01-14 14:00:11 +0300

    test

--- src/lib/chains.ts
+++ src/lib/chains.ts
@@ -1,3 +1,4 @@
+import { defineChain } from 'viem'
 import * as chains from 'viem/chains'
 import type { Chain } from 'viem/chains'
 
@@ -18,5 +19,22 @@ for (const [k, v] of Object.entries(chains)) {
   chainMap[k] = v
   allChains.push(v)
 }
+
+allChains.push(defineChain({
+  id: 2741,
+  name: 'abstract',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Abstract Mainnet',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://api.raas.matterhosted.dev'],
+    },
+  },
+  contracts: {
+  },
+}))
 
 export { chainMap, allChains }
